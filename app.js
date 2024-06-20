ac6fc50e39b97431cb413d1959b3ec2f973a7da8
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-06-20 10:35:56 +0300

    Add event to the previous button and add prev class to the carousel class

--- app.js
+++ app.js
@@ -27,11 +27,22 @@ function showSlider(type) {
     thumbnail.appendChild(thumbnailItem[0]);
 
     carousel.classList.add("next"); // this adds the next class to the carousel and implements the css style animations written for the next class
+  }else{
+    let indexOfLastItem = listItem.length-1;
+
+    //When prev class on carousel is active and when prev button is clicked we use prepend method
+    //The prepend method allows you to insert another element at the beginning of an element in JavaScript.
+    list.prepend(listItem[indexOfLastItem]);
+    thumbnail.prepend(thumbnailItem[indexOfLastItem]);
+    carousel.classList.add('prev');
   }
+
+
   clearTimeout(runTimeOut);//Each time the showSlider function is run, this ensures the countdown time always starts from the beginning
   runTimeOut =setTimeout(()=> {
     // this will remove the next class from the carousel class
     carousel.classList.remove('next');
+    carousel.classList.remove('prev');
   },timeRunning)
 }
 
